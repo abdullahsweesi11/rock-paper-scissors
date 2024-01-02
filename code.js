b1216ff6f75df9e5ff6168e5577991c89b427d9a
abdullahsweesi11 <85199062+abdullahsweesi11@users.noreply.github.com>
2024-01-02 22:18:22 +0000

    Implement GUI interface to the Game

--- code.js
+++ code.js
@@ -1,105 +1,111 @@
 
-start = document.querySelector('.start')
+const start = document.querySelector('.start')
+const reload = document.querySelector('.reload')
 
-start.addEventListener('click', function() {
+const options = document.querySelectorAll('button.option')
+const detail = document.querySelector('.detail')
+let playerScore = document.querySelector('#player>.score')
+let playerChoice = document.querySelector('#player-choice')
+let computerScore = document.querySelector('#computer>.score')
+let computerChoice = document.querySelector('#computer-choice')
 
-    function getComputerChoice() {
+let result;
 
-        let options = ['Rock', 'Paper', 'Scissors']
+console.log(options)
 
-        let variable = Math.floor(Math.random() * 3);
-
-        return options[variable]
-    }
-
-    let playRound = function(playerSelection, computerSelection) {
-
-        try {
-            playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
-
-            if (['Rock', 'Paper', 'Scissors'].includes(playerSelection)) {
-                console.log(`You chose: ${playerSelection}. Computer chose: ${computerSelection}`)
-            }
-            
-            if (playerSelection === computerSelection) {
-                return playRound(prompt('Enter your choice:'), getComputerChoice())
-            }
-            else if (playerSelection === 'Rock') {
-                switch (computerSelection) {
-                    case 'Paper':
-                        return 'You lose! Paper beats Rock'
-                        break
-                    case 'Scissors':
-                        return 'You win! Rock beats Scissors'
-                }
-            }
-            else if (playerSelection === 'Paper') {
-                switch (computerSelection) {
-                    case 'Rock':
-                        return 'You win! Paper beats Rock'
-                    case 'Scissors':
-                        return 'You lose! Scissors beats Paper'
-                }
-            }
-            else if (playerSelection === 'Scissors') {
-                switch (computerSelection) {
-                    case 'Rock':
-                        return 'You lose! Rock beats Scissors'
-                        break
-                    case 'Paper':
-                        return 'You win! Scissors beats Paper'
-                }
-            }
-            else {
-                return 'Please make sure your input is valid'
-            }
-        }
-        catch (e) {
-            return 'Please enter "Rock", "Paper", or "Scissors' // catching any inputs that would trip up the case-insensitivity functionality
-        }
-        
-    }
-
-    function game() {
-        console.clear()
-
-        let computerScore = 0;
-        let playerScore = 0;
-
-        for (let i = 0; i < 5; i++) {
-
-            input = prompt('Enter your choice:')
-
-            if (input == null) {
-                console.log('Gamed Ended')
-                break
-            }
-            
-            result = playRound(input, getComputerChoice())
-
-            if (result.includes('win')) {
-                playerScore++
-                console.log(`You win round ${i + 1}`)
-                console.log(`Player: ${playerScore}. Computer: ${computerScore}`)
-            }
-            else if (result.includes('lose')) {
-                computerScore++
-                console.log(`Computer wins round ${i + 1}`)
-                console.log(`Player: ${playerScore}. Computer: ${computerScore}`)
-            }
-            else {
-                console.log(result)
-                i--
-            }
-
-            if (playerScore === 3 || computerScore === 3) {
-                console.log(`${playerScore === 3 ? 'Player' : 'Computer'} wins the game!`)
-                break
-            }
-
-        }
-        
-    }
-
-    game()
+start.addEventListener('click', function (event) {
+    event.target.style.display = 'none'
+    document.querySelector('#game-screen').style.display = 'block'
 })
+
+reload.addEventListener('click', function() {
+    location.reload()
+})
+
+
+function getComputerChoice() {
+
+    let options = ['Rock', 'Paper', 'Scissors']
+
+    let variable = Math.floor(Math.random() * 3);
+
+    computerChoice.textContent = options[variable]
+
+    return options[variable]
+}
+
+let playRound = function(playerSelection, computerSelection) {
+    console.log(playerSelection)
+
+        playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
+
+        
+        
+        if (playerSelection === computerSelection) {
+            return "That was a draw!"
+        }
+        else if (playerSelection === 'Rock') {
+            switch (computerSelection) {
+                case 'Paper':
+                    return 'You lose this round! Paper beats Rock'
+                    break
+                case 'Scissors':
+                    return 'You win this round! Rock beats Scissors'
+            }
+        }
+        else if (playerSelection === 'Paper') {
+            switch (computerSelection) {
+                case 'Rock':
+                    return 'You win this round! Paper beats Rock'
+                case 'Scissors':
+                    return 'You lose this round! Scissors beats Paper'
+            }
+        }
+        else if (playerSelection === 'Scissors') {
+            switch (computerSelection) {
+                case 'Rock':
+                    return 'You lose this round! Rock beats Scissors'
+                    break
+                case 'Paper':
+                    return 'You win this round! Scissors beats Paper'
+            }
+        }
+    
+}
+
+function checkGame() {
+
+        if (playerScore.textContent === '5' || computerScore.textContent === '5') {
+            document.querySelector('.disable').style.display = 'block'
+            document.querySelector('#winner').textContent = playerScore.textContent === '5' ? 'PLAYER' : 'COMPUTER'
+
+        }
+        
+}
+
+options.forEach( (option) => {
+    option.addEventListener('click', function(event) {
+    
+    result = playRound(option.value, getComputerChoice())
+    playerChoice.textContent = option.value
+
+    if (result.includes('win')) {
+        console.log(+playerScore.textContent)
+        playerScore.textContent = (+(playerScore.textContent) + 1).toString()
+        detail.textContent = "Player Won!"
+        
+    }
+    else if (result.includes('lose')) {
+        console.log(+computerScore.textContent)
+        computerScore.textContent = (+(computerScore.textContent) + 1).toString()
+        detail.textContent = "Computer Won!"
+        
+    }
+    else {
+        detail.textContent = result
+    }
+
+    checkGame()
+
+})}
+)
